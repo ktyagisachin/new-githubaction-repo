8b04a4b1d398e3dbf8b0c89e9bca58159a33b545
sachin Tyagi <ktyagi.sachin@gmail.com>
2026-05-13 10:59:40 +0530

    RG code change

--- Resourcegroup/main.tf
+++ Resourcegroup/main.tf
@@ -1,15 +1,20 @@
 
-resource "azurerm_resource_group" "rg--new-var" {
-    for_each = var.resource_group
-    name     = each.value.name
-    location = each.value.location
-}
+# resource "azurerm_resource_group" "rg--new-var" {
+#     for_each = var.resource_group
+#     name     = each.value.name
+#     location = each.value.location
+# }
 
 
-resource "azurerm_resource_group" "rg--new-count" {
-    count=2
-    name     = "nw-rg-office${count.index}"
-    location = "eastus"
-}
+# resource "azurerm_resource_group" "rg--new-count" {
+#     count=2
+#     name     = "nw-rg-office${count.index}"
+#     location = "eastus"
+# }
+ resource "azurerm_resource_group" "rg--new-action" {
+
+     name     = "nw-rg-gitaction"
+     location = "eastus"
+ }
 
 
